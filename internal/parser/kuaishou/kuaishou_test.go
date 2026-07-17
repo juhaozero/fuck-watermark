178cf835478205fb2e16fd0dedafe79a00a1e6b9
juhaozero <56676157+juhaozero@users.noreply.github.com>
2026-07-17 11:06:44 +0800

    feat: 快手测试新增

---
+++ internal/parser/kuaishou/kuaishou_test.go
@@ -0,0 +1,25 @@
+package kuaishou
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"fuck-watermark/internal/httputil"
+	"fuck-watermark/internal/parser"
+	"testing"
+	"time"
+)
+
+func TestParser_Parse(t *testing.T) {
+	client := httputil.New(30 * time.Second)
+	p := New(client)
+	resp := p.Parse(context.Background(), parser.Request{
+		URL: "https://www.kuaishou.com/f/X7XA0IJXjQwr5mQ",
+	})
+	if resp.Code != 200 {
+		t.Errorf("Parse failed: %v", resp.Msg)
+	}
+	jsonData, _ := json.Marshal(resp.Data)
+	fmt.Println("parse kuaishou video response: ", string(jsonData))
+	t.Logf("Parse response: %v", string(jsonData))
+}
